e3c9b824896c44a8150979a85da2e87ebf292d3a
dnguyen2 <mdnguyen123321@gmail.com>
2025-03-04 20:54:45 -0500

    Fourth Commit for paint dry code. Add extra explanations

--- PaintDryTimer.cpp
+++ PaintDryTimer.cpp
@@ -4,6 +4,8 @@
 #include <vector> // for vectors (duh)
 #include <cstdlib> // for random
 #include <cassert> // for assert in the tests() function
+#include <chrono> // for testing
+#include <thread>
 #include "TimeCode.h" // for timecode's (duh)
 #include "TimeCode.cpp"
 
@@ -51,25 +53,45 @@ void tests(){
 	assert(ans > 6 && ans < 8);
 	// add more tests here
 
+    // testing for sth that just started drying
+    dss.startTime = time(0);
+    TimeCode tc3 = TimeCode(0, 0, 10);
+    dss.timeToDry = &tc3;
+    this_thread::sleep_for(chrono::seconds(1));
+    ans = get_time_remaining(dss);
+    assert(ans > 8 && ans < 10);
+
+    // testing for sth that finished drying 
+    dss.startTime = time(0) - 10; 
+    TimeCode tc4 = TimeCode(0, 0, 10);
+    dss.timeToDry = &tc4;
+    ans = get_time_remaining(dss);
+    assert(ans == 0); 
 
 	// get_sphere_sa
 	double sa = get_sphere_sa(2.0);
 	assert (50.2654 < sa && sa < 50.2655);
 	// add more tests here
 
+    sa = get_sphere_sa(0.0);
+    assert(sa == 0.0);
+
+    sa = get_sphere_sa(1000.0);
+    assert(12566370.0 < sa && sa < 12566371.0);
 
 	// compute_time_code
 	TimeCode *tc2 = compute_time_code(1.0);
 	//cout << "tc: " << tc.GetTimeCodeAsSeconds() << endl;
 	assert(tc2->GetTimeCodeAsSeconds() == 1);
-	delete tc2;
-
 
 	// add more tests here
 
+    TimeCode *tc5 = compute_time_code(1000000.0);
+    assert(tc5->GetTimeCodeAsSeconds() == 1000000);
+    delete tc2;
+    delete tc5;
 
 	cout << "ALL TESTS PASSED!" << endl;
-
 }
 
 
@@ -91,7 +113,7 @@ int main(){
     while (get_time_remaining(dss) > 0) {
         cout << "Time remaining: " << get_time_remaining(dss) << " seconds\r";
         cout.flush();
-        // this_thread::sleep_for(chrono::seconds()); //Delay for 1 seconds
+        this_thread::sleep_for(chrono::seconds()); //Delay for 1 seconds
     }
 
     cout << "\nPaint has dried!" << endl;
